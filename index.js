b1d18d0a08665553ac17ea2cb708ceba55a67923
Jagoda Ługowska <61515165+JLugowska@users.noreply.github.com>
2025-06-05 23:04:54 +0200

    Add files via upload

---
+++ index.js
@@ -0,0 +1,47 @@
+const mqtt = require('mqtt');
+const { MongoClient } = require('mongodb');
+
+const {
+  MQTT_HOST,
+  MQTT_PORT,
+  MQTT_USER,
+  MQTT_PASS,
+  MQTT_TOPIC,
+  MONGO_URI,
+  MONGO_DB,
+  MONGO_COLLECTION
+} = process.env;
+
+const client = mqtt.connect(`mqtt://${MQTT_HOST}:${MQTT_PORT}`, {
+  username: MQTT_USER,
+  password: MQTT_PASS
+});
+
+const mongoClient = new MongoClient(MONGO_URI);
+
+client.on('connect', () => {
+  console.log('Połączono z MQTT');
+  client.subscribe(MQTT_TOPIC, (err) => {
+    if (err) {
+      console.error('Błąd subskrypcji:', err);
+    }
+  });
+});
+
+client.on('message', async (topic, message) => {
+  try {
+    const data = JSON.parse(message.toString());
+    const { napięcie1, napięcie2, prąd } = data;
+    const moc = napięcie1 * prąd;
+    const timestamp = new Date();
+
+    await mongoClient.connect();
+    const db = mongoClient.db(MONGO_DB);
+    const collection = db.collection(MONGO_COLLECTION);
+
+    await collection.insertOne({ napięcie1, napięcie2, prąd, moc, timestamp });
+    console.log('Dane zapisane do MongoDB');
+  } catch (error) {
+    console.error('Błąd przetwarzania wiadomości:', error);
+  }
+});
